2cf76c00f57445045dfa1c96a521fca01460517c
Shawna Sanjay <shawnasanjay150@gmail.com>
2025-12-03 13:52:20 -0800

    made win and lose

--- buzzer_test.py
+++ buzzer_test.py
@@ -8,27 +8,46 @@ from gpiozero import TonalBuzzer
 from gpiozero.tones import Tone
 
 tb = TonalBuzzer(23)
-tone_A3 = Tone(note= "A3")
-tone_G5 = Tone(note= "G5")
+tA3 = Tone(note= "A3")
+tA4 = Tone(note= "A#4")
+tA5 = Tone(note= "A5")
+tC4 = Tone(note= "C4")
+tC5 = Tone(note= "C5")
+tF4 = Tone(note= "F4")
+
+def lose_sfx():
+    print("You Lose")
+    tb.play(tC4)
+    sleep(0.1)
+    tb.play(tF4)
+    sleep(0.5)
+    tb.play(tF4)
+    sleep(0.5)
+    tb.stop()
+    
+def win_sfx():
+    print("You Win")
+    tb.play(tA3)
+    sleep(0.1)
+    tb.play(tC5)
+    sleep(0.1)
+    tb.play(tC5)
+    sleep(0.3)
+    tb.play(tA4)
+    sleep(0.5)
+    tb.play(tA3)
+    tb.stop()
+    
 
 try:
-    while True:
-        tb.play(tone_G5)
-        print("You Win")
-        sleep(0.5)
-        
-        tb.stop()
-        print("no beep")
-        sleep(0.5)
-        
-        tb.play(tone_A3)
-        print("You Lose")
-        sleep(0.5)
-        
-        tb.stop()
-        print("no beep")
-        sleep(0.5)
-        
+    game_won = False
+
+    if game_won:
+        win_sfx()
+    else:
+        lose_sfx()
+    sleep(1)
+    
 except KeyboardInterrupt:
     print("Exiting...")
 
